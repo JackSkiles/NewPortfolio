ad4ee9143294abb94f2b64196ef5bc6808a8571b
Jack Skiles <jackskiles420@gmail.com>
2022-01-06 11:14:09 -0500

    practice with react components in next.js

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div>
+            <p>Copyright 2021 Jack Skiles</p>
+        </div>
+     );
+}
+ 
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../comps/Navbar'
+import Footer from '../comps/Footer'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Aute aliqua irure esse tempor eu cillum nulla veniam. Laborum sunt voluptate laboris tempor pariatur anim nisi nisi dolor. Nulla consequat in
         duis enim aliquip laboris eiusmod id cupidatat mollit cillum laboris excepteur aliquip. Consectetur aliquip proident est non minim id do voluptate excepteur et laboris cillum deserunt esse.
@@ -12,6 +15,7 @@ export default function Home() {
       <p>Aute aliqua irure esse tempor eu cillum nulla veniam. Laborum sunt voluptate laboris tempor pariatur anim nisi nisi dolor. Nulla consequat in
         duis enim aliquip laboris eiusmod id cupidatat mollit cillum laboris excepteur aliquip. Consectetur aliquip proident est non minim id do voluptate excepteur et laboris cillum deserunt esse.
       </p>
+      <Footer />
     </div>
   )
 }
