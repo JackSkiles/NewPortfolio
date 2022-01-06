b2b13e7c18c142a42a4f6bd664deeaa769e1dac2
Jack Skiles <jackskiles420@gmail.com>
2022-01-06 10:13:43 -0500

    added new about page for testing

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Aute aliqua irure esse tempor eu cillum nulla veniam. Laborum sunt voluptate laboris tempor pariatur anim nisi nisi dolor. Nulla consequat in 
+               duis enim aliquip laboris eiusmod id cupidatat mollit cillum laboris excepteur aliquip. Consectetur aliquip proident est non minim id do voluptate excepteur et laboris cillum deserunt esse.
+            </p>
+            <p>Aute aliqua irure esse tempor eu cillum nulla veniam. Laborum sunt voluptate laboris tempor pariatur anim nisi nisi dolor. Nulla consequat in 
+               duis enim aliquip laboris eiusmod id cupidatat mollit cillum laboris excepteur aliquip. Consectetur aliquip proident est non minim id do voluptate excepteur et laboris cillum deserunt esse.
+            </p>
+        </div>
+     );
+}
+ 
+export default About;
