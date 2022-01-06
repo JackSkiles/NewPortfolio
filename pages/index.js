c4030de33abab07e3f9c05a827208acca1960e8b
Jack Skiles <jackskiles420@gmail.com>
2022-01-06 12:32:38 -0500

    playing around with global styles

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
+import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Homepage</h1>
       <p>Aute aliqua irure esse tempor eu cillum nulla veniam. Laborum sunt voluptate laboris tempor pariatur anim nisi nisi dolor. Nulla consequat in
         duis enim aliquip laboris eiusmod id cupidatat mollit cillum laboris excepteur aliquip. Consectetur aliquip proident est non minim id do voluptate excepteur et laboris cillum deserunt esse.
