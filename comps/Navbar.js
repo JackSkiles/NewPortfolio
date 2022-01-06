a6abb538b385ebb5824a28210586495c1e57a639
Jack Skiles <jackskiles420@gmail.com>
2022-01-06 11:30:44 -0500

    added links

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,12 +1,16 @@
+import Link from 'next/link'
+
 const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
                 <h1>Ninja List</h1>
             </div>
-            <a>Home</a>
-            <a>About</a>
-            <a>Ninja Listing</a>
+            <Link href="/"><a>Home</a></Link>
+            <br/>
+            <Link href="/about"><a>About</a></Link>
+            <br/>
+            <Link href="/subpages"><a>Sub Pages</a></Link>
         </nav>
      );
 }
