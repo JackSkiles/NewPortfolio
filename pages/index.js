0dd3205e591d2a0bb72511b9c00f876bdc0ada70
Jack Skiles <jackskiles420@gmail.com>
2022-01-06 12:36:35 -0500

    some name changes and style additions

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,16 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Homepage</h1>
-      <p>Aute aliqua irure esse tempor eu cillum nulla veniam. Laborum sunt voluptate laboris tempor pariatur anim nisi nisi dolor. Nulla consequat in
+    <div >
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Aute aliqua irure esse tempor eu cillum nulla veniam. Laborum sunt voluptate laboris tempor pariatur anim nisi nisi dolor. Nulla consequat in
         duis enim aliquip laboris eiusmod id cupidatat mollit cillum laboris excepteur aliquip. Consectetur aliquip proident est non minim id do voluptate excepteur et laboris cillum deserunt esse.
       </p>
-      <p>Aute aliqua irure esse tempor eu cillum nulla veniam. Laborum sunt voluptate laboris tempor pariatur anim nisi nisi dolor. Nulla consequat in
+      <p className={styles.text}>Aute aliqua irure esse tempor eu cillum nulla veniam. Laborum sunt voluptate laboris tempor pariatur anim nisi nisi dolor. Nulla consequat in
         duis enim aliquip laboris eiusmod id cupidatat mollit cillum laboris excepteur aliquip. Consectetur aliquip proident est non minim id do voluptate excepteur et laboris cillum deserunt esse.
       </p>
       <Link href="/subpages">
-        <a>See Sub Pages</a>
+        <a className={styles.btn}>See Sub Pages</a>
       </Link>
     </div>
   )
