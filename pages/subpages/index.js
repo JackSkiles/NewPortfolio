892eb06091eee40581871c1e15d28f2337744575
Jack Skiles <jackskiles420@gmail.com>
2022-01-10 22:30:19 -0500

    lots of routing additions

--- pages/subpages/index.js
+++ pages/subpages/index.js
@@ -1,7 +1,27 @@
-const Subpages = () => {
+import styles from '../../styles/subpages.module.css'
+import Link from 'next/link';
+
+export const getStaticProps = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    return {
+        props: { subpages: data }
+    }
+
+}
+
+const Subpages = ({ subpages }) => {
     return ( 
         <div>
             <h1>All Ninjas</h1>
+            {subpages.map(page => (
+                <Link href={'/subpages/' + page.id} key={ page.id }>
+                    <a className={styles.single}>
+                        <h3>{ page.name }</h3>
+                    </a>
+                </Link>
+            ))}
         </div>
      );
 }
